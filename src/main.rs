8cd4cb53171292339ecacffb10c8752b470f661b
TaiseiIto <taisei1212@outlook.jp>
2026-04-06 20:03:27 +0900

    Create a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Clone, Debug, Default)]
+struct Message();
+
+#[derive(Clone, Debug, Default)]
+struct State();
+
+fn update(_: &mut State, _: Message) -> iced::Task<Message> {
+    iced::Task::none()
+}
+
+fn view(_: &State) -> iced::Element<'_, Message> {
+    iced::widget::column![].into()
+}
+
+fn main() -> iced::Result {
+    iced::run(update, view)
 }
